aecc7f70011d9c1e6c327560e359e33c890f8b94
Roy González Alemán <roy.gonzalez.aleman@gmail.com>
2023-09-25 00:23:50 +0200

    [NF] Add NUCLEAR tests

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,7 +1,6 @@
 # Created by roy.gonzalez-aleman at 17/09/2023
-import filecmp
 import pickle
-from os.path import join, dirname, abspath
+from os.path import join, dirname, getsize
 
 import pytest
 
@@ -19,9 +18,6 @@ def unpickle_from_file(file_name):
     return data
 
 
-tests_dir = abspath(dirname(__file__))
-
-
 @pytest.fixture()
 def reference_and_target_fixture(request):
     cwd = dirname(request.module.__file__)
@@ -37,28 +33,35 @@ def reference_and_target_fixture(request):
 
 
 def test_cases_correspondence(reference_and_target_fixture):
+    # assert same data will be compared
     reference_data, target_data = reference_and_target_fixture
     assert set(target_data.keys()) == set(reference_data.keys())
 
 
 def test_files_correspondence(reference_and_target_fixture):
+    # assert produced files are the same
     reference_data, target_data = reference_and_target_fixture
-
     for key in reference_data:
-        # assert produced files are the same
         assert key in target_data
 
 
 def test_files_identity(reference_and_target_fixture):
+    # assert all files are identical
     reference_data, target_data = reference_and_target_fixture
+
     for key in reference_data:
-        # assert all files are identical
         gs_files = reference_data[key]
         target_files = target_data[key]
+
         for gs_file in gs_files:
             gs_file_path = gs_files[gs_file]
             target_file_path = target_files[gs_file]
-            if 'nuclear.cfg' not in gs_file_path and 'html' not in gs_file_path:
-                assert filecmp.cmp(gs_file_path, target_file_path,
-                                   shallow=False), \
-                    f'\n{target_file_path} offending identity comparison.'
+            sms1 = f'File {target_file_path} has different size than reference.'
+            sms2 = f'File {target_file_path} is not identical to reference.'
+
+            if ('nuclear.cfg' not in gs_file_path) and (
+                    'html' not in gs_file_path):
+                assert getsize(gs_file_path) == getsize(target_file_path), sms1
+                gs_opened = open(gs_file_path, 'rb').read()
+                target_opened = open(target_file_path, 'rb').read()
+                assert gs_opened == target_opened, sms2
